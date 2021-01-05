9180836545cdd78717b7a3370e0beb6fe9571033
SkarletA <65414016+SkarletA@users.noreply.github.com>
2021-01-05 16:43:06 -0600

    Revert "ejercicio de leer numero e inprimir en forma de triangulo"

--- triangulo.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 17.- Escribe un programa que lea un número e imprime un triángulo de números de la
-#forma siguiente. Si el número leído es 4, imprimirá:
-
-numero = int ( input (" Ingresa un numero: "))
-
-for i in range(1, numero+1):
-    for j in range(1, i+1):
-        print(str(i), end=" ")
-    print()
