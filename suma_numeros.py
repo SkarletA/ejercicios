d232d6c61b4106c94451903fd2e80fb1598d20e1
Skarlet Araque <skarlet15m@gmail.com>
2021-02-16 11:24:41 -0600

    Arreglando la representacion de la suma

--- suma_numeros.py
+++ suma_numeros.py
@@ -1,9 +1,8 @@
 """Ejercicios 37.- Obtener diez números (0..9) mediante el generador de números aleatorios. Con
 estos diez números se formarán dos de cinco cifras cada uno.
 Sumar los dos números de cinco cifras obtenidos. La suma se realizará cifra a cifra
-(unidades, decenas, centenas, etc.), apareciendo los sumandos y el suma en letra,
-también se indicará en letra si te llevas una y aparecerá un "1" encima del sumando
-correspondiente."""
+(unidades, decenas, centenas, etc.), apareciendo los sumandos,
+también se indicará si llevas una y aparecerá un "1" encima del sumando correspondiente."""
 
 import random
 
@@ -22,32 +21,43 @@ for i in range(5, 10):
     numero_dos.append(n[i])   
 
 
+
 resultado = [0, 0, 0, 0, 0]
-carry = [0, 0, 0, 0, 0]
+carries = [0, 0, 0, 0, 0]
 for j in range(4, -1, -1):
-    suma = numero_uno[j]+numero_dos[j]+carry[j]
+    suma = numero_uno[j]+numero_dos[j]+carries[j]
     if suma > 9:
         if j == 0:
             resultado[j] = suma
         else:
             resultado[j] = suma % 10
-            carry[j-1]  = suma // 10
+            carries[j-1]  = suma // 10
     else:
         resultado[j] = suma
 
+print("La siguiente es una representacion de la suma: ")
 
-for j in range(4, -1, -1):
-    print(carry[j], end ="   ")
-print()
 
-for j in range(4, -1, -1):
+def sustituir_ceros(carry):
+    if carry == 0:
+        return " "
+    else:
+        return f"{carry}"
+
+print("   ".join(map(sustituir_ceros, carries)))
+
+
+
+for j in range(5):
     print(numero_uno[j], end = "   ")
 print()
 
-for j in range(4, -1, -1):
+for j in range(5):
     print(numero_dos[j], end ="   ")
 print()
 
-for j in range(4, -1, -1):
+print("------------------")
+
+for j in range(5):
     print(resultado[j], end ="   ")
 print()
